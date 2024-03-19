eed4dee67069ed73bc8e8d8c0987bfc0f9865728
jaeyoonyeom111215 <155369925+jaeyoonyeom111215@users.noreply.github.com>
2024-03-19 23:16:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 
-title = "고민, 학급 건의사항 등을 적어주십시오."
+title = "의견이 전송되었습니다."
 
 st.header("1-7 학급 소통함")
 st.subheader(title)
@@ -9,7 +9,6 @@ st.subheader(title)
     
 user_input = st.text_input("의견을 입력하세요.")
 if st.button("submit"):
-    st.header("의견이 전송되었습니다.")
+    st.header(title)
     time.sleep(1)
-    del st.text_input("")
-    del st.header("의견이 전송되었습니다.")
+    title = ""
