a71a4289bd5695f97d6c91d168b1748a607bc982
jaeyoonyeom111215 <155369925+jaeyoonyeom111215@users.noreply.github.com>
2024-03-19 23:03:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import time
 
 title = "고민, 학급 건의사항 등을 적어주십시오."
 
-def main():
-    st.title("1-7 학급 소통함")
-    st.subheader(title)
+st.header("1-7 학급 소통함")
+st.subheader(title)
 
     
 user_input = st.text_input("의견을 입력하세요.")
 if st.button("submit"):
-    st.subheader("의견이 성공적으로 전송되었습니다.")
+    st.text_input("의견이 전송되었습니다.")
     time.sleep(1)
-    st.subheader(title)
+    st.text_input("")
