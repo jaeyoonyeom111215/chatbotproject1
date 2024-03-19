f7b932803be9adb2b05435b37ef663397fb091a3
jaeyoonyeom111215 <155369925+jaeyoonyeom111215@users.noreply.github.com>
2024-03-19 23:21:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ title = "의견이 전송되었습니다."
 
 st.header("1-7 학급 소통함")
 
-user_input = st.text_input("의견을 입력하세요.")
+def clear_text():
+    st.session_state["text"] = ""
+
+input = st.text_input("text", key="text")    
+st.button("clear text input", on_click=clear_text)
+st.write(input)
 if st.button("submit"):
     st.header(title)
     time.sleep(1)
