c91a49e765fe805bb299448ed11f5ebe3a3b0368
jaeyoonyeom111215 <155369925+jaeyoonyeom111215@users.noreply.github.com>
2024-03-20 12:36:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import time
-from streamlit_gsheets import GSheetsConnection
-
-[connections.gsheets]
-spreadsheet = "https://docs.google.com/spreadsheets/d/1fvsxY8b0mWXQbeTJpZ79RJGHhaMPutBEairMeDBB8Ps/edit?usp=drivesdk"
 
 title = "의견이 전송되었습니다."
 
