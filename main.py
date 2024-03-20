8d1857c36227eefbe3079ebf0d992be0f49a2fd5
jaeyoonyeom111215 <155369925+jaeyoonyeom111215@users.noreply.github.com>
2024-03-20 12:32:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import time
+from streamlit_gsheets import GSheetsConnection
+
+[connections.gsheets]
+spreadsheet = "https://docs.google.com/spreadsheets/d/1fvsxY8b0mWXQbeTJpZ79RJGHhaMPutBEairMeDBB8Ps/edit?usp=drivesdk"
 
 title = "의견이 전송되었습니다."
 
 st.header("1-7 학급 소통함")
 
-def clear_text():
-    st.session_state["text"] = ""
 
 input = st.text_input("text", key="text")    
-st.button("clear text input", on_click=clear_text)
+
 st.write(input)
 if st.button("submit"):
     st.header(title)
     time.sleep(1)
-    title = ""
+    st.session_state["text"] = ""
